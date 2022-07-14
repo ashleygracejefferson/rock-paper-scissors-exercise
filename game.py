83b3b3d812a864632c0becaa2bd17068782f588f
ashleygracejefferson <agj7854@stern.nyu.edu>
2022-07-13 22:59:43 -0400

    Validations for graceeful failure

--- game.py
+++ game.py
@@ -2,5 +2,10 @@
 
 print("Welcome 'Player One' to my Rock, Paper, Scissors, game!")
 
-x=input("Please select rock. paper, or scissors:")
+x=input("Please select rock. paper, or scissors:").lower()
 print(x)
+
+if x!="rock" and x!="paper" and x!="scissors":
+    print("Please try again") 
+    exit()
+
